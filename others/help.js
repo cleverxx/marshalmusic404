a34038897f2fce1ecfbaacd74d8737df191cd54d
Dev_KaYa <93824968+Dev1KaYa@users.noreply.github.com>
2021-11-17 01:58:50 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,25 +15,26 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setThumbnail(`https://media.discordapp.net/attachments/861399487233654834/872561176456884224/image0.png `)
+    .setThumbnail(` `)
     .setTitle(`**Random Help**`)
     .setDescription(`
-<a:emoji_180:856878164592427028>  **User Commands**
-**__invite - support- about - ping - prefix - uptime - tinvites - sug - roleuser - report__**
+<a:emoji_180:856878164592427028>  User / Commands
+invite / support / about / ping / prefix / uptime / tinvites / sug / roleuser / report
 
-<a:emoji_102:871813008471507004> **Music Commands**
-**__play - skip - skipto - stop - volume  nowplaying - shuffle - search  - loop - lyrics -radio__**
+<a:emoji_102:871813008471507004> Music Commands
+play / skip / skipto / stop / volume / nowplaying / shuffle / search  / loop / lyrics / radio
 
-<a:emoji_11:845102475740577792> **Gif Commands**
-**__boy - girl - couple - sad - cartoon - anime - neon__**
+<a:emoji_11:845102475740577792> Gif Commands
+boy / girl / couple / sad - cartoon / anime / neon 
 
-<a:FBBE600B13DB4F5986B4D0AF89247091:871811080148639794>  **Moderation Commands**
-**__ban - unban - setnick - roleadd - roleremove - vkick - setprefix - clear - close - open - senddm__**
+<a:FBBE600B13DB4F5986B4D0AF89247091:871811080148639794>  /Moderation Commands
+ban / unban / setnick / roleadd / roleremove / vkick / setprefix / clear / close / open / senddm
 
-<a:kass:872897079305986108> **Game Commands**
-**__kiss - slap - hug - slots__**
 
- <a:emoji_103:871813076964507710> **Links**
+<a:kass:872897079305986108> Game Commands
+kiss / slap / hug / slots
+
+ <a:emoji_103:871813076964507710> Links
  [Support](https://discord.gg/K8SSqweuSH)
  [Invite](https://discord.com/api/oauth2/authorize?client_id=802995353209012234&permissions=8&scope=bot)
  [Website](coming soon)
