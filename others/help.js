5391ef1a21a8debb64f43a970c74a73f4ea1de5f
Dev_KaYa <93824968+Dev1KaYa@users.noreply.github.com>
2021-11-17 02:07:46 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,26 +18,31 @@ module.exports = {
     .setThumbnail(` `)
     .setTitle(`**Random Help**`)
     .setDescription(`
-<a:emoji_180:856878164592427028>  User / Commands
-invite / support / about / ping / prefix / uptime / tinvites / sug / roleuser / report
+> <a:emoji_180:856878164592427028>  User / Commands
+> invite / support / about / ping / prefix /
+> uptime / tinvites / sug / roleuser / report
 
-<a:emoji_102:871813008471507004> Music Commands
-play / skip / skipto / stop / volume / nowplaying / shuffle / search  / loop / lyrics / radio
+> <a:emoji_102:871813008471507004> Music Commands
+> play / skip / skipto / stop / volume / nowplaying /
+> shuffle / search  / loop / lyrics / radio
 
-<a:emoji_11:845102475740577792> Gif Commands
-boy / girl / couple / sad - cartoon / anime / neon 
+> <a:emoji_11:845102475740577792> Gif Commands
+> boy / girl / couple / sad 
+> / cartoon / anime / neon 
 
-<a:FBBE600B13DB4F5986B4D0AF89247091:871811080148639794>  /Moderation Commands
-ban / unban / setnick / roleadd / roleremove / vkick / setprefix / clear / close / open / senddm
+> <a:FBBE600B13DB4F5986B4D0AF89247091:871811080148639794>  /Moderation Commands
+> ban / unban / setnick / roleadd / roleremove / 
+> vkick / setprefix / clear / close / open / senddm
 
 
 <a:kass:872897079305986108> Game Commands
-kiss / slap / hug / slots
+kiss / slap /
+hug / slots
 
- <a:emoji_103:871813076964507710> Links
- [Support](https://discord.gg/K8SSqweuSH)
- [Invite](https://discord.com/api/oauth2/authorize?client_id=802995353209012234&permissions=8&scope=bot)
- [Website](coming soon)
+>  <a:emoji_103:871813076964507710> Links
+ > [Support](https://discord.gg/K8SSqweuSH)
+ > [Invite](https://discord.com/api/oauth2/authorize?client_id=802995353209012234&permissions=8&scope=bot)
+ > [Website](coming soon)
 `)
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM");
