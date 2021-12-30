94d294f1456a3c1f863727718d48bbf922fcc081
cleverxx <95709607+cleverxx@users.noreply.github.com>
2021-12-30 17:37:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login("OTE5NjY1MzgwMjcwNjczOTIx.YbZHIQ.NPXL7Uh0IiG4o-BiowomczMlblM");
+client.login("OTE5NjY1MzgwMjcwNjczOTIx.YbZHIQ.RSCyM7dKSTTiiX7Px-6ed8v6zeI");
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
